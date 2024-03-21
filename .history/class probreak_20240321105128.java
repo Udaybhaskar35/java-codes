713df8990dfd50f388f7110aec235d7065c05d66
udaybhaskar35 <udaybhaskarrao35@gmail.com>
2024-03-21 11:04:10 +0530

    code kk

---
+++ .history/class probreak_20240321105128.java
@@ -0,0 +1,15 @@
+class probreak
+{
+    public static void main(String args[])
+    {
+        for(int  i=o;i<=10;i++)
+        {
+            System.out.println(i);
+            if(i==5)
+            break;
+            System.out.println("using break");
+
+        }
+        System.out.println("out of loop");
+    }
+}
---
+++ code1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class code1
+{
+    public static void main(String args[])
+    {
+        int a = 5;
+        int b = a + 5;
+        System.out.println("the addition is "+ b);
+
+    }
+}
